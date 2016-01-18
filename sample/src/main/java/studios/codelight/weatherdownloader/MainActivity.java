24801deb6d6e1f89ef9e30f8766fbe2ad45c0849
kalyandechiraju <kalyandechiraju@outlook.com>
2016-01-18 23:22:35 +0530

    Improve Sample app

--- sample/src/main/java/studios/codelight/weatherdownloader/MainActivity.java
+++ sample/src/main/java/studios/codelight/weatherdownloader/MainActivity.java
@@ -102,16 +102,16 @@ public class MainActivity extends AppCompatActivity implements WeatherDownloader
     public void onWeatherDownloadComplete(WeatherData data, WeatherDownloader.Mode mode) {
         if (mode == WeatherDownloader.Mode.CITYNAME) {
             cityNameProgress.setVisibility(View.GONE);
-            cityNameResult.setText(WeatherUnits.convertToCelsius(data.getMain().getTemp()).toString());
+            cityNameResult.setText(String.format("%.2f", WeatherUnits.convertToCelsius(data.getMain().getTemp())));
         } else if (mode == WeatherDownloader.Mode.CITYID) {
             cityIdProgress.setVisibility(View.GONE);
-            cityIdResult.setText(data.getMain().getTemp());
+            cityIdResult.setText(String.format("%.2f", WeatherUnits.convertToFahrenheit(data.getMain().getTemp())));
         } else if (mode == WeatherDownloader.Mode.COORDINATES) {
             coordinatesProgress.setVisibility(View.GONE);
             coordinatesResult.setText(data.getMain().getTemp());
         } else if (mode == WeatherDownloader.Mode.ZIPCODE) {
             zipCodeProgress.setVisibility(View.GONE);
-            zipCodeResult.setText(data.getMain().getTemp());
+            zipCodeResult.setText(String.format("%.2f", WeatherUnits.convertToFahrenheit(data.getMain().getTemp())));
         }
     }
 
